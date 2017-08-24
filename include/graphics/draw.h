c05fe00549cb2eca7b9427c0f3e90ec1d7ac15a9
s179122 <s179122@SEA-8E412E>
2017-08-23 17:57:13 -0700

    make shapes

--- include/graphics/draw.h
+++ include/graphics/draw.h
@@ -12,6 +12,30 @@ void setUniform(const Shader &shader, int location, float value);
 
 void clearFramebuffer(const FrameBuffer &fb);
 
-void setUniform(const Shader &shader, int location, int value, int press);
+void setUniform(const Shader &s, int location, int value, int press);
+
 
 void setUniform(const Shader &s, int location, const Texture &value, int slot);
+
+namespace __internal
+{
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, float val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, int val);
+	void t_setUniform(const Shader &s, int &loc_io, int &tex_io, const Texture &val);
+
+
+}
+
+template<typename T, typename ...U>
+void setUniforms(const Shader &s, int &loc_io, int &tex_io, const T &val, U &&...uniforms)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+	setUniforms(s, loc_io, tex_io, uniforms...);
+}
+
+template<typename T>
+void setUniforms(const Shader &s, int &loc_io,int &tex_io, const T &val)
+{
+	__internal::t_setUniform(s, loc_io, tex_io, val);
+}
+
--- source/draw.cpp
+++ source/draw.cpp
@@ -1,6 +1,8 @@
 #include "graphics\RenderObjects.h"
 #include "glinc.h"
 #include "..\include\graphics\draw.h"
+#include "glm\glm.hpp"
+#include "glm\gtc\type_ptr.hpp"
 
 void s0_draw(const FrameBuffer & f, const Shader & s, const Geometry & g)
 {
@@ -35,5 +37,43 @@ void setUniform(const Shader & s, int location, const Texture & value, int slot)
 	glActiveTexture(GL_TEXTURE0 + slot);
 	glBindTexture(GL_TEXTURE_2D, value.handle);
 	glProgramUniform1i(s.handle, location, slot);
+
+}
+
+namespace __internal
+{
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, float val)
+	{
+		glProgramUniform1f(s.handle, loc_io++, val);
+	}
+
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, int val)
+	{
+		glProgramUniform1i(s.handle, loc_io++, val);
+
+	}
+
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, const Texture & val)
+	{
+		glActiveTexture(GL_TEXTURE0 + tex_io);
+		glBindTexture(GL_TEXTURE_2D, val.handle);
+		glProgramUniform1i(s.handle, loc_io++, tex_io++);
+	}
+
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, const glm::vec3 & val)
+	{
+		glProgramUniform3fv(s.handle, loc_io++, 1, glm::value_ptr(glm::vec3(0, 0, 1)));
+	}
+
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, const glm::vec4 & val)
+	{
+		glProgramUniform4fv(s.handle, loc_io++, 1, glm::value_ptr(val));
+	}
+
+	void t_setUniform(const Shader & s, int & loc_io, int & tex_io, const glm::mat4 & val)
+	{
+		glProgramUniformMatrix4fv(s.handle, loc_io++, 1, 0, glm::value_ptr(val));
+	}
+
 
 }
